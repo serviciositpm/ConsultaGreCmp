33ec9926bfb8455d5eed954e90a7b63e324249e4
bareca90 <bareca90@gmail.com>
2024-07-16 15:49:21 -0500

    Segundo Commit , se procede a crear archivos de configuración y validar que se conecte a la base de datos

---
+++ app.js
@@ -0,0 +1,41 @@
+const sql               =   require('mssql/msnodesqlv8');
+const cron              =   require('node-cron');
+const config            =   require('./config/configDb');
+//--------------------------------------------
+//Validacion que se COnecte a la BD
+//--------------------------------------------
+const mensaje = async()=>{
+    try{
+        const respuesta = await sql.connect(config)
+        if (respuesta) console.log('Se Conecto a la Base Correctamente')
+        return true ;
+    }catch(error){
+        console.log(error)
+        return false ;
+    }
+}
+const main = async () => {
+    //--------------------------------------------
+    //Validación Base de Datos
+    //--------------------------------------------
+    const val_conexion = await mensaje();
+    if (val_conexion){
+        //--------------------------------------------
+        //Ejecución de el Cron de Consulta
+        //--------------------------------------------
+        try {
+            //const msg   = await processRecords();
+            console.log('Ingreso por el dato correcto');
+            //cron.schedule('* * * * *', processRecords);
+            //console.log(msg);
+        } catch (error) {
+            console.error('Error al Ejecutar Procesos:', error);
+        }
+        
+    }else{
+        console.log('Revise configuracion de BD');
+    }
+
+};
+
+main();
---
+++ config/configDb.js
@@ -0,0 +1,15 @@
+const config={
+    user: 'user',
+    password: '84+-blaster32',
+    server: '10.100.120.8',
+    database: 'pSipe4',
+    driver: 'msnodesqlv8',
+    
+    options: {
+        enableArithAbort: true,
+        trustedConnection: true
+    }
+    
+}
+
+module.exports = config;
