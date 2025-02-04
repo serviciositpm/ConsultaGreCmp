c1abe6de84571bddc36be914c0de0239a3dbb30c
Bairon Reyes <37020508+bareca90@users.noreply.github.com>
2025-02-04 09:06:36 -0500

    Cambiar parametros de repeticion del cron

--- app.js
+++ app.js
@@ -27,8 +27,8 @@ const main = async () => {
         try {
             //const msg   = await processRecords();
             /* console.log('Ingreso por el dato correcto'); */
-            //cron.schedule('*/5 * * * *', processRecords);
-            cron.schedule('* * * * *', processRecords);
+            cron.schedule('*/5 * * * *', processRecords);
+            //cron.schedule('* * * * *', processRecords);
             //console.log(msg);
         } catch (error) {
             console.error('Error al Ejecutar Procesos:', error);
