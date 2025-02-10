b83bfa36cdf37290b0debe08f5c80914228de823
Bairon Reyes <37020508+bareca90@users.noreply.github.com>
2025-02-10 11:54:57 -0500

    <Feat><models.js><Se Procede a realizar el cambio para que tome lo que indica zona de pesca>

--- sql/models.js
+++ sql/models.js
@@ -47,14 +47,18 @@ const insertDataOnTablePesca = async (objetoJsonInTrack,spname) => {
         for (const row of objetoJsonInTrack) {
             const mainRow = row.main_row;
             const subRows = row.sub_rows;
-            /*
-            * Se Valida que vengan siempre datos de Programa de Pesca : Grupoing(Dispostivo) = subRow[0] ,Guia = subRow[2], Geocerca = subRow[4]
-            */
-            if (mainRow[0] != '' && mainRow[3] != '' && mainRow[4].t != '' && mainRow[5].t != '') {
-                const datosinsertados = await insertDataSpGreCmpPescas(mainRow[0], mainRow[1], mainRow[2], mainRow[3], mainRow[4].t, mainRow[5].t, mainRow[6], mainRow[7],mainRow[8],mainRow[9].t,mainRow[10], spname);
-            }
+            for (const subRow of subRows) {
+                /*
+                * Se Valida que vengan siempre datos de Programa de Pesca : Grupoing(Dispostivo) = subRow[0] ,Guia = subRow[2], Geocerca = subRow[4]
+                */
+                if (subRow[0] != '' && subRow[3] != '' && subRow[4].t != '' && subRow[5].t != '' && subRow[9].t.startsWith('ZP-')) {
+                    //const datosinsertados = await insertDataSpGreCmp(subRow[0], subRow[1], subRow[2], subRow[3], subRow[4], subRow[5].t, subRow[6], subRow[7].t, subRow[8], subRow[9], subRow[10], subRow[11].t, subRow[12], spname);
+                    const datosinsertados = await insertDataSpGreCmpPescas(subRow[0], subRow[1], subRow[2], subRow[3], subRow[4].t, subRow[5].t, subRow[6], subRow[7],subRow[8],subRow[9].t,subRow[10], spname);
 
-           
+                    //console.log(datosinsertados);
+                }
+
+            }
         }
     
     } catch (error) {
